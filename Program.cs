1f0e9d8cac4b166ede985023f226d92192fe19a4
unknown <kate.mudrow@snow.edu>
2025-08-28 19:59:44 -0600

    finished step:1 integer math

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+int a = 18;
+int b = 6;
+int c = a * b;
+Console.WriteLine(c);
