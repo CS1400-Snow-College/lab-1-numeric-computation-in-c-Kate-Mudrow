864b9d82e92ba4cb4da73f2487802b79dc219b88
unknown <kate.mudrow@snow.edu>
2025-08-28 21:48:51 -0600

    Finished step 5:Challenge

--- Program.cs
+++ Program.cs
@@ -1,15 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-double a = 15;
+double a = 19;
 double b = 9;
-double c = 7;
-double d = (a + b) / c;
-Console.WriteLine(d);
-
-double max = double.MaxValue;
-double min = double.MinValue;
-Console.WriteLine($"The range of double value is {min} to {max}");
-
-double ninth = 1.0 / 9.0;
-Console.WriteLine(ninth);
+double c = 14;
+double d = 55;
+double e = (a + b) / c + 15 - b + 5 + (a + d) * 18 + a / d;
+Console.WriteLine(e);
