9a765f30b5fd26e22873eadbe81333745e8284b0
unknown <kate.mudrow@snow.edu>
2025-08-28 20:07:11 -0600

    finished step 2:OO

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-int a = 18;
-int b = 6;
-int c = a * b;
-Console.WriteLine(c);
+int a = 10;
+int b = 4;
+int c = 5;
+int d = a + b * c;
+Console.WriteLine(d);
