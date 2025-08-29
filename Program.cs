83dacf33ab57782e6aa7134fe66f78e5816e8898
unknown <kate.mudrow@snow.edu>
2025-08-28 21:04:45 -0600

    Finished step 4:QR

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-int a = 7;
+int a = 9;
 int b = 4;
-int c = 3;
+int c = 5;
 int d = (a + b) / c;
-Console.WriteLine(d);
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
