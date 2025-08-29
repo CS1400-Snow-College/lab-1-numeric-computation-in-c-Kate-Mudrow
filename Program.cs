fad60e8b4cc16e0eba5a58a6edde1296914368b3
unknown <kate.mudrow@snow.edu>
2025-08-28 21:37:13 -0600

    Finished step 5:Double

--- Program.cs
+++ Program.cs
@@ -1,17 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-int a = 9;
-int b = 4;
-int c = 5;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
-
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
-
-int what = max + 5;
-Console.WriteLine($"example of overflow: {what}");
+double a = 15;
+double b = 9;
+double c = 7;
+double d = (a + b) / c;
+Console.WriteLine(d);
