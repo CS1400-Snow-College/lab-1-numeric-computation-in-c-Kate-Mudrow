e02ab002c8073eeda566de5d78e6b4e423aa668b
unknown <kate.mudrow@snow.edu>
2025-08-28 21:56:54 -0600

    Finished step 6:Decimal Range

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-double a = 19;
-double b = 9;
-double c = 14;
-double d = 55;
-double e = (a + b) / c + 15 - b + 5 + (a + d) * 18 + a / d;
-Console.WriteLine(e);
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
